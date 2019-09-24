01e11f9a17a2ddf3c7d5321c07bc154a0fd136fa
Murilo <muriloarrudap@gmail.com>
2019-09-24 00:31:42 -0300

    add content and style

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,49 +1,96 @@
-import { Link, graphql } from 'gatsby';
 import React from 'react';
-import { container } from './header.module.scss';
+import { Link, graphql, useStaticQuery } from 'gatsby';
+import {
+  Linkedin,
+  Twitter,
+  GitHub,
+  MessageCircle,
+  User,
+  BookOpen,
+  Box,
+} from 'react-feather';
 import Img from 'gatsby-image';
+
+import {
+  container,
+  profilePic,
+  profileCard,
+  description,
+  socialList,
+} from './header.module.scss';
 
 interface Props {
   siteTitle?: string;
   data: any;
 }
-export const query = graphql`
-  query {
-    file(relativePath: { eq: "profile_beta.png" }) {
-      childImageSharp {
-        # Specify the image processing specifications right in the query.
-        fluid {
-          ...GatsbyImageSharpFluid
+
+const Header: React.FC<Props> = ({ siteTitle }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "profile_beta.png" }) {
+        childImageSharp {
+          # Specify a fixed image and fragment.
+          # The default width is 400 pixels
+          fixed(width: 100, height: 100) {
+            ...GatsbyImageSharpFixed
+          }
         }
       }
     }
-  }
-`;
-const Header: React.FC<Props> = ({ data }) => {
-  const siteTitle = 'Murilo';
-  console.log('data', data);
+  `);
+
   return (
     <header className={container}>
       <div>
-        <Img fluid={data.file.childImageSharp.fluid} />
-        <h1>
-          <Link to="/">{siteTitle}</Link>
-        </h1>
-        <p>Desenvolvedor full-stack</p>
+        <div className={profileCard}>
+          <Img
+            className={profilePic}
+            alt="foto de perfil"
+            fixed={data.file.childImageSharp.fixed}
+          />
+          <h1>
+            <Link to="/">{siteTitle}</Link>
+          </h1>
+          <p>Desenvolvedor full-stack</p>
+          <p className={description}>
+            Programador em constante aprendizagem. Aqui vou compartilhar minhas
+            experiências e conhecimentos na área.
+          </p>
+        </div>
         <ul>
           <li>
-            <Link to="/blog">Blog</Link>
+            <Link to="/blog">
+              {' '}
+              <BookOpen color="#ff2079" size={18} /> Blog
+            </Link>
           </li>
           <li>
-            <Link to="/about">Sobre Mim</Link>
+            <Link to="/about">
+              <User color="#ff2079" size={18} /> Sobre Mim
+            </Link>
           </li>
           <li>
-            <Link to="/projetos">Projetos</Link>
+            <Link to="/projetos">
+              <Box color="#ff2079" size={18} /> Projetos
+            </Link>
           </li>
           <li>
-            <Link to="/contact">Contato</Link>
+            <Link to="/contact">
+              <MessageCircle color="#ff2079" size={18} /> Contato
+            </Link>
           </li>
         </ul>
+        <div className={socialList}>
+          <a href="https://www.linkedin.com/in/muriloarruda/" target="_blank">
+            <Linkedin />
+          </a>
+          <a href="https://twitter.com/passocabr" target="_blank">
+            <Twitter />
+          </a>
+          <a href="https://github.com/murilo-arruda" target="_blank">
+            <GitHub />
+          </a>
+        </div>
       </div>
     </header>
   );
